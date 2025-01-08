70f38b16f5f3a421d01e85ca45dbbb8b8d00ab78
jsrojoo <jsimon.rojo@gmail.com>
2025-01-08 07:27:32 -0800

    updates

--- keymap.c
+++ keymap.c
@@ -8,15 +8,25 @@ const  uint16_t  PROGMEM  _rthumb_combo[]  =  {OSL(1),  KC_K,  COMBO_END};
 const  uint16_t  PROGMEM  _uy_combo[]      =  {KC_U,    KC_Y,  COMBO_END};         
 const  uint16_t  PROGMEM  _wf_combo[]      =  {KC_W,    KC_F,  COMBO_END};         
 const  uint16_t  PROGMEM  _rsei_combo[]    =  {KC_R,    KC_S,  KC_E,        KC_I,  COMBO_END};
+const  uint16_t  PROGMEM  _rs_combo[]    =  {KC_R,    KC_S,  COMBO_END};
+const  uint16_t  PROGMEM  _wfuy_combo[]    =  {KC_W,    KC_F,  KC_U,        KC_Y,  COMBO_END};
+
+const  uint16_t  PROGMEM  _fplu_combo[]    =  {KC_F,    KC_P,  KC_L, KC_U, COMBO_END};
 
 combo_t key_combos[] = {
-	COMBO(  _lthumb_combo,  OSM(MOD_LSFT)  ),
-	COMBO(  _rthumb_combo,  OSM(MOD_LSFT)  ),
 
-	COMBO(  _uy_combo,      OSM(MOD_LSFT)  ),
-	COMBO(  _wf_combo,      OSM(MOD_LSFT)  ),
+	COMBO(  _lthumb_combo,         OSM(MOD_LCTL)  ),
+	COMBO(  _rthumb_combo,         OSM(MOD_LCTL)  ),
 
-	COMBO(  _rsei_combo,    KC_CAPS        ),
+	COMBO(  _uy_combo,             OSM(MOD_LSFT)  ),
+	COMBO(  _wf_combo,             OSM(MOD_LSFT)  ),
+	COMBO(  _rs_combo,             OSM(MOD_LSFT)  ),
+
+	COMBO(  _rsei_combo,           KC_CAPS        ),
+	COMBO(  _wfuy_combo,           KC_CAPS        ),
+
+	COMBO(  _fplu_combo,           TO(2)          ),
+
 };
 
 void leader_start_user(void) {
@@ -73,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT(
 		   KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, 							KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
 
-		   KC_NO, KC_ESC, KC_NO, KC_UP, KC_NO, KC_NO,						 	KC_NO, KC_7, KC_8, KC_9, KC_0, KC_NO, 
+		   KC_NO, KC_ESC, KC_NO, KC_UP, KC_NO, KC_NO,						 	KC_NO, KC_7, KC_8, KC_9, KC_0, KC_BSPC, 
 
 		   TO(0), KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, 						KC_TRNS, LCTL_T(KC_4), LSFT_T(KC_5), LALT_T(KC_6), LGUI_T(KC_0), KC_NO, 
 
