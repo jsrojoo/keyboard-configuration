4dfa5019e0d3026f9f02128682e984f424a1ccea
Joseph Rojo <joseph.rojo@fiserv.com>
2026-02-06 19:31:26 +0800

    Update

--- keymap.c
+++ keymap.c
@@ -108,11 +108,11 @@ void leader_end_user(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
 
-		    KC_GRV, KC_NO, KC_DQUO, RALT_T(KC_QUOT), TO(2), KC_NO,                       KC_NO, TO(5), OSM(MOD_LALT), OSM(MOD_LGUI), TO(4), QK_LEAD,
+		    KC_GRV, KC_1, RGUI_T(KC_2), RALT_T(KC_3), KC_4, KC_5,                       KC_6, KC_7, OSM(MOD_LALT), OSM(MOD_LGUI), KC_9, KC_0,
 
-		    KC_TAB, KC_Q, KC_W, LT(1, KC_F), KC_P, KC_G,                                 KC_J, KC_L, LT(1, KC_U), KC_Y, KC_SCLN, KC_BSPC,
+		    KC_TAB, KC_Q, KC_W, LT(1, KC_F), KC_P, KC_G,                                KC_J, KC_L, LT(1, KC_U), KC_Y, KC_SCLN, KC_BSPC,
 
-		    KC_ESC, KC_A, LSFT_T(KC_R), LT(2, KC_S), LSFT_T(KC_T), KC_D,	               KC_H, LSFT_T(KC_N), LT(2, KC_E), LSFT_T(KC_I), KC_O, KC_ENT,
+		    LSFT_T(KC_ESC), KC_A, KC_R, LT(2, KC_S), KC_T, KC_D,	                      KC_H, KC_N, LT(2, KC_E), KC_I, KC_O, LSFT_T(KC_QUOT),
 
 		    KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LT,					                    KC_GT,	KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
 
@@ -132,13 +132,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		   ),
 
     [2] = LAYOUT(
-		   KC_GRV, KC_NO, KC_DQUO, KC_QUOT, KC_NO, KC_NO,		                        KC_NO, KC_NO, OSM(MOD_LALT), OSM(MOD_LGUI), KC_NO, QK_LEAD,
+		   KC_GRV, KC_NO, KC_DQUO, KC_QUOT, KC_NO, KC_NO,		                          KC_NO, KC_NO, OSM(MOD_LALT), OSM(MOD_LGUI), KC_NO, QK_LEAD,
 
-		   KC_NO, KC_ESC, KC_ESC, KC_UP, KC_SPC, KC_NO,						 	                    KC_1, KC_3, KC_5, KC_7, KC_9, KC_BSPC, 
+		   KC_NO, KC_ESC, KC_ESC, KC_UP, KC_SPC, KC_NO,						 	                  KC_1, KC_3, KC_5, KC_7, KC_9, KC_BSPC, 
 
 		   TO(0), KC_ENT, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, 						              KC_2, KC_4, KC_6, KC_8, KC_0, KC_ENT,
 
-		   QMK_BASE_LAYER, KC_NO, KC_NO, KC_NO, KC_SPC, KC_SPC, KC_LT,				 KC_GT, KC_ENT, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
+		   QMK_BASE_LAYER, KC_NO, KC_NO, KC_NO, KC_SPC, KC_NO, KC_LT,				 KC_GT, KC_NO, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
 
 		    					LT(5,KC_PSCR), TO(3), TO(0), KC_SPC,		 KC_ENT, TO(1), TO(2), TO(3)
 		   ),
